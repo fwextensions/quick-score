9d6b86ce2f518ad3ce67a2b6e245997f27a19daf
John Dunning <fw@johndunning.com>
2021-01-02 12:59:45 -0800

    Remove extraneous code from transform-string.test.js
    Update devDependencies to the latest, fixing a vulnerability in jest.

--- test/transform-string.test.js
+++ test/transform-string.test.js
@@ -13,10 +13,7 @@ describe("Use transformString()", () => {
 	test("Uppercase transformString() still matches all queries", () => {
 		const lowercaseQS = new QuickScore(Strings);
 		const uppercaseQS = new QuickScore(Strings, { transformString });
-const items = ["café", "cafeteria"];
-const qs = new QuickScore(items, { transformString: s => latinize(s).toLowerCase() });
-const results = qs.search("cafe");
-console.log();
+
 		expect(lowercaseQS.search("gh").length).toBe(3);
 		expect(uppercaseQS.search("gh").length).toBe(3);
 		expect(lowercaseQS.search("GH").length).toBe(3);
